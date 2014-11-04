e84357c6a4c8f11b5ee416b7aab1701bb70da0bb
gaogao-9 <gaoutuki@gmail.com>
2014-11-04 16:32:41 +0900

    Create jquery-multiinput.js

---
+++ jquery-multiinput.js
@@ -0,0 +1,194 @@
+//サンプル
+//$(document).on("multistart",function(eve){
+//	console.log(eve.type);      // 押したデバイスに依存[mousedown/touchstart/pointerdown]いずれか
+//	console.log(eve.multiType); // "multistart"と表示
+//	
+//	var pos = $.fn.getMultiXY(eve); // この関数もこのライブラリに入ってます。
+//	console.log("page  (" + pos.pageX   + "," + pos.pageY   + ")"); // "page(x,y)"と表示
+//	console.log("client(" + pos.clientX + "," + pos.clientY + ")"); // "client(x,y)"と表示
+//	console.log("screen(" + pos.screenX + "," + pos.screenY + ")"); // "screen(x,y)"と表示
+//	↑の3つはデバイスに依存せず常に返します。touchの場合は、changedTouches[0]を元に返します。
+//	
+//	依存先を変えたい場合は$.fn.getMultiXY(eve,{target:num});で、numを指定すると変えられます。
+//	numが無指定もしくは0のときはchangedTouched依存
+//	numが1のときはtargetTouches依存
+//	numが2のときはtouches依存となっています。
+//});
+//});
+
+!function($){
+	var eventsCnt = 0;
+	var callbackEvents = [];
+	var wrapEvents   = [];
+	var multiEvents    = [];
+	var device  = null;
+	var devices = ["pointer","mouse","touch"];
+	var types   = ["start","move","end","enter","leave"];
+	var events  = {
+		start  : "pointerdown mousedown touchstart",
+		move   : "pointermove mousemove touchmove",
+		end    : "pointerup mouseup touchend",
+		enter  : "pointerenter mouseenter touchenter",
+		leave  : "pointerleave mouseleave touchleave"
+	};
+	
+	//multiイベントの種類に応じたpointer/mouse/touchイベントを追加する関数
+	var AddMultiEvent = function(self,type,dele,callback){
+		var MultiEvent = function(self,type,cnt){
+			var match;
+			var multiType = "multi" + type;
+			return function(eve){
+				eve.multiType = multiType;
+				
+				match = devices.filter(function(ele){
+					return ~eve.type.indexOf(ele);
+				});
+				if(device!==match[0]){
+					if(device==="touch"){
+						//touch中に他のデバイスが発動したら無視する
+						return;
+					}
+					if(device!==null){
+						//前回値が不明でなく、前回と今回が違う場合、
+						//このマルチイベントはすでに呼ばれている。
+						device = match[0];
+						return;
+					}
+					device = match[0];
+				}
+				
+				$(self).triggerHandler(multiType,[eve]);
+			};
+		};
+		
+		var multiEvent = MultiEvent(self,type,eventsCnt);
+		multiEvents[eventsCnt]    = multiEvent;
+		callbackEvents[eventsCnt] = callback;
+		if(dele===null){
+			$(self).on(events[type],multiEvent);
+		}
+		else{
+			$(self).on(events[type],dele,multiEvent);
+		}
+		return eventsCnt++;
+	};
+	
+	//オーバーライドたのしい！！
+	var _on = $.fn.on;
+	$.fn.on = function(a,b,c,d,e){
+		switch(a){
+			case "multistart":
+			case "multimove":
+			case "multiend":
+			case "multienter":
+			case "multileave":
+				var id,_dele,_callback,_wrap;
+				var match = types.filter(function(ele){
+					return ~a.indexOf(ele);
+				});
+				_wrap = function(hnd,eve){
+					eve.multiType = hnd.type;
+					return _callback.apply(this,[eve]);
+				};
+				if(typeof b==="function"){
+					_dele = null;
+					_callback = b;
+					b = _wrap;
+				}
+				else if(typeof c==="function"){
+					_dele = b;
+					_callback = c;
+					c = _wrap;
+				}
+				else{
+					throw new Error("引数が不正だよっ");
+				}
+				id = AddMultiEvent(this,match[0],_dele,_callback);
+				wrapEvents[id] = _wrap;
+				break;
+		}
+		return _on.apply(this,[a,b,c,d,e]);
+	};
+	
+	var _off = $.fn.off;
+	$.fn.off = function(a,b,c){
+		switch(a){
+			case "multistart":
+			case "multimove":
+			case "multiend":
+			case "multienter":
+			case "multileave":
+				var id,match,_multi,_wrap;
+				if(typeof b==="function"){
+					id = callbackEvents.indexOf(b);
+					if(id<0) break;
+					b = wrapEvents[id];
+					_multi = multiEvents[id];
+					match = types.filter(function(ele){
+						return ~a.indexOf(ele);
+					});
+					$(this).off(events[match[0]],_multi);
+				}
+				else if(typeof c==="function"){
+					id = callbackEvents.indexOf(c);
+					if(id<0) break;
+					c = wrapEvents[id];
+					_multi = multiEvents[id];
+					match = types.filter(function(ele){
+						return ~a.indexOf(ele);
+					});
+					$(this).off(events[match[0]],_multi);
+				}
+				if(typeof _multi==="function"){
+					delete callbackEvents[id];
+					delete wrapEvents[id];
+					delete multiEvents[id];
+				}
+				break;
+		}
+		return _off.apply(this,[a,b,c]);
+	};
+	
+	//mouse,touch間の座標の差異を吸収するなんとか
+	$.fn.getMultiXY = function(eve,opt){
+		var output = {};
+		if(!eve) return output;
+		
+		opt = opt || {};
+		opt.target = isNaN(opt.target) ? 0 : Math.max(0,Math.min(opt.target-0,2));
+		var cTarget = ["changedTouches","targetTouches","touches"][opt.target];
+		
+		switch(eve.type){
+			case "mousedown":
+			case "mousemove":
+			case "mouseup":
+			case "mouseenter":
+			case "mouseleave":
+			default: //pointerイベントが実装され始めたらちゃんと書く
+				output.screenX = eve.screenX;
+				output.screenY = eve.screenY;
+				output.clientX = eve.clientX;
+				output.clientY = eve.clientY;
+				output.pageX   = eve.pageX;
+				output.pageY   = eve.pageY;
+				break;
+			case "touchstart":
+			case "touchmove":
+			case "touchend":
+			case "touchenter":
+			case "touchleave":
+			case "touchcancel":
+				var cTouches = eve.originalEvent[cTarget];
+				if(!cTouches || !cTouches.length) return output;
+				output.screenX = cTouches[0].screenX;
+				output.screenY = cTouches[0].screenY;
+				output.clientX = cTouches[0].clientX;
+				output.clientY = cTouches[0].clientY;
+				output.pageX   = cTouches[0].pageX;
+				output.pageY   = cTouches[0].pageY;
+				break;
+		}
+		
+		return output;
+	};
+}(jQuery);
